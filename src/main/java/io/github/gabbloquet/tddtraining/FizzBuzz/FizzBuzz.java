835c8c4e52e2c11207a8dda1cabc02691b418089
Kdouma25 <kdouma@sqli.com>
2025-03-13 11:49:32 +0000

    Update FizzBuzz.java

--- src/main/java/io/github/gabbloquet/tddtraining/FizzBuzz/FizzBuzz.java
+++ src/main/java/io/github/gabbloquet/tddtraining/FizzBuzz/FizzBuzz.java
@@ -1,26 +1,33 @@
 package io.github.gabbloquet.tddtraining.FizzBuzz;
 
 public class FizzBuzz {
-  public String convert(int input) {
-    if(input % 15 == 0){
-      return "FizzBuzz";
-    } else if(input % 5 == 0) {
-      return "Buzz";
-    } else if(input % 3 == 0){
-      return "Fizz";
-    }
-    return String.valueOf(input);
+  public String convert(int number) {
+      String toReturn = "";
+
+      if (number % 3==0 || number % 5==0) {
+          if (number % 3==0) {
+              toReturn += "Fizz";
+          }
+          if (number % 5 ==0){
+              toReturn +=  "Buzz";
+          }
+       }else{ //  not divisible by either 3 nor 5
+          toReturn += String.valueOf(number);
+      }
+
+      return toReturn;
   }
 
-  public String compute(int to) {
-    if(to < 1)
-      throw new NonCompliantNumberException();
 
-    StringBuilder transformedString = new StringBuilder();
-    for(int i = 1; i <= to; i++){
-      transformedString.append(convert(i));
-    }
-
-    return transformedString.toString();
+  public String compute(int number) throws NonCompliantNumberException {
+      if(number <= 0){
+          throw new NonCompliantNumberException();
+      }
+      String output = "";
+      for (int i=1;i<=number;i++){
+            output += convert(i);
+      }
+      return output;
   }
+
 }
